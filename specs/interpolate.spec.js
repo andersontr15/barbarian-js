8ff8a67cf7605ec94876b2b87cdf8de581a2e582
Theo Anderson <theoanderson3@gmail.com>
2017-02-08 23:37:44 -0500

    fix spacing

--- specs/interpolate.spec.js
+++ specs/interpolate.spec.js
@@ -6,28 +6,28 @@ const interpolate = require('../index').interpolate;
 
 // sample test
 test('it runs a test', () => {
-	expect(typeof 'hello world').toBe('string')
+ expect(typeof 'hello world').toBe('string')
 });
 
 // testing interpolation functionality of controllers 
 
 test('it interpolates 1 expression from Barbarian controllers', () => {
-		var view = '<h1>{name}</h1>';
-		var template = document.createElement('div');
-		template.innerHTML = view;
-		template.setAttribute('barbarian-controller', 'Test');
-		document.body.appendChild(template);		
-		var mockController = {
-			name: 'Test',
-			properties: {
-			name: 'Theo'
-			}
-		}
-		var expected = 'Theo'
+  var view = '<h1>{name}</h1>';
+  var template = document.createElement('div');
+  template.innerHTML = view;
+  template.setAttribute('barbarian-controller', 'Test');
+  document.body.appendChild(template);  
+  var mockController = {
+   name: 'Test',
+   properties: {
+   name: 'Theo'
+   }
+  }
+  var expected = 'Theo'
 
-	 var result =	interpolate(mockController);
+  var result = interpolate(mockController);
 
-		expect(result).toBe(expected);
+  expect(result).toBe(expected);
 })
 
 }());
