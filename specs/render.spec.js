e8ffb41effbeaac3c7d4a898e1259b6a2fcb6a16
Theo <theoanderson3@gmail.com>
2017-02-10 17:03:30 -0500

    clean up tests

--- specs/render.spec.js
+++ specs/render.spec.js
@@ -1,7 +1,9 @@
 (function() {
    const render = require('../src/index').render;
    describe('It tests for basic functionality of the Barbarian render method', () => {
+
     beforeAll(() => {
+
      this.tagName = 'h1';
      this.content = 'I am an h1';
      this.element = {
@@ -13,16 +15,22 @@
 
      this.insertAfter = 'div';
      this.expectedTextContent = 'I am an h1';
-     this.expectedOuterHTML = '<h1>' + this.expectedTextContent + '</h1>'
+     this.expectedOuterHTML = '<h1>' + this.expectedTextContent + '</h1>';
+
     });
+
      test("it creates and renders a simple h1 element with text", () => {
+
         var result =  render(this.element, this.insertAfter);
         expect(result.textContent).toBe(expectedTextContent);
     });
+
     test("it creates and returns an h1 element with the correct outerHTML", () => {
+
        var result = render(this.element, this.insertAfter);
        expect(result.outerHTML).toBe(this.expectedOuterHTML)
     });
+
    });
 
 }());
