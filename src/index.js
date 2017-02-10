85c02f62699485200503329ab8deeee2ca9863fb
Theo <theoanderson3@gmail.com>
2017-02-10 17:33:57 -0500

    add render method to master

--- src/index.js
+++ src/index.js
@@ -2,22 +2,29 @@
   'use strict';
   var modules = [];
   var interpolationMatches = ['{', '}'];
-
+  var saveData = function(data) {
+    window.localStorage.setItem(data.name, JSON.stringify(data))
+  }
   var Router = function(options) {
       return {
-        routes: [],
+        routes: options.routes || [],
         previousRoute: null,
         currentRoute: window.location.href,
         routeTo: function(path) {
-          this.currentRoute = path;
-        },
-        routesHistory: [],
-        withHash: false
+          var route = this.routes.filter(r => r.path === path)[0];
+          if(route) {
+            history.pushState({ controller: route.controller } , '', route.path);
+            window.localStorage.setItem('controller', route.controller);
+            document.querySelector('h3').innerHTML = route.path;
+          }
+          else {
+            throw new Error('No route found with this name')
+          }
+        }
       }
     }
   var Barbarian = {
     Router: Router,
-    // custom interpolation method
     interpolate: function(controller) {
       if(controller === undefined || !controller.name) {
         throw new Error('Must be a valid controller!')
@@ -25,13 +32,20 @@
       var hasBinding = function(element) {
         return element.textContent.indexOf(interpolationMatches[0]) > -1 && element.textContent.indexOf(interpolationMatches[1]) > -1
       }
-      var startPoint = Array.from(document.querySelectorAll('[barbarian-controller =' + controller.name + ']'));
+      var startPoint = Array.from(document.querySelectorAll('[barbarian-controller=' + controller.name + ']'));
+      if(startPoint.length < 2) {
+        var element = startPoint[0];
+        if(hasBinding(element) === true) {
+          var value = element.textContent.slice(element.textContent.indexOf('{') + 1, element.textContent.lastIndexOf('}'));
+          if(value !== null && controller.properties.hasOwnProperty(value)){
+            element.textContent = controller.properties[value];
+            return element.textContent;
+          }
+        }
+      }
       startPoint.forEach(function(c) {
         if(c !== null) {
-          // lets bind some properties!
           var children = Array.from(c.children);
-
-          // loop through node list and interpolate
           children.forEach(function(child) {
             if(hasBinding(child) === true) {
               var value = child.textContent.slice(child.textContent.indexOf('{') + 1, child.textContent.lastIndexOf('}'));
@@ -67,8 +81,6 @@
     },
     makeController: function(name, properties) {
       var controller = {
-        // trim controller name so as to allow for white space without 
-        // throwing an exception
         name: name.trim(),
         properties: properties,
         dependencies: properties.dependencies,
@@ -83,16 +95,24 @@
       var selector = object.selector;
       var bindTo = object.bindTo;
       var properties = object.properties;
-
       var element = document.createElement(selector);
       element.textContent = text;
       for(var prop in properties) {
         element.setAttribute('data-' + prop, properties[prop])
       }
       var elementToBindTo = document.querySelector(bindTo);
-
       elementToBindTo.appendChild(element);
       return element;
+    },
+    render: function(element, insertAfter) {
+      var newElement = document.createElement(element.tagName);
+      newElement.textContent = element.content;
+      var nodeToAppendTo = document.querySelector(insertAfter);
+      nodeToAppendTo.appendChild(newElement);
+      return {
+        outerHTML: document.querySelector(element.tagName).outerHTML,
+        textContent: document.querySelector(element.tagName).textContent
+      }
     }
   };
   window.Barbarian = Barbarian;
